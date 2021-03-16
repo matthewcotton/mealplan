9d6653e3fe13da0b45c4dea76538d8e16c82b749
levilu800b <levilu800b@gmail.com>
2021-03-16 12:57:17 +0000

    Dashboard

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -9,46 +9,54 @@ class Dashboard extends React.Component {
         <Container>
           <Row>
             <Col>
-      <Card className="cards">
-  <Card.Body>
-  <button className="button">
-    <Link className="link"  to="users">Add Recipe</Link>              
-</button>
-  </Card.Body>
-</Card>
-</Col>
-<Col>
-<Card className="cards">
-  <Card.Body>
-  <button className="button">
-    <Link className="link" to="users">View Recipes</Link>              
-</button>
-  </Card.Body>
-</Card>
-</Col>
-</Row>
-<Row>
-            <Col>
-      <Card className="cards">
-  <Card.Body>
-  <button className="button">
-    <Link className="link" to="users">Create Meal Plan</Link>              
-</button>
-  </Card.Body>
-</Card>
-</Col>
-<Col>
-<Card className="cards">
-  <Card.Body>
-  <button className="button" variant="primary">
-    <Link className="link" to="users">View Meal Plans</Link>              
-</button>
-  </Card.Body>
-</Card>
-</Col>
-</Row>
-</Container>
-      </div>
+      <Card className="card">
+        <Card.Img variant="top" className="card-img"
+        src="https://images.unsplash.com/photo-1526383426426-c1716158d22a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" />
+        <Card.Body className="card-body">
+          <Card.Link className="card-link">
+          <Link className="link" to="users">Add Recipe</Link>  
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      </Col>
+      <Col>
+      <Card className="card">
+        <Card.Img variant="top" className="card-img"
+        src="https://images.unsplash.com/photo-1526383426426-c1716158d22a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" />
+        <Card.Body className="card-body">
+          <Card.Link className="card-link">
+          <Link className="link" to="users">Add Recipe</Link>  
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      </Col>
+      </Row>
+      <Row>
+        <Col>
+      <Card className="card">
+        <Card.Img variant="top" className="card-img"
+        src="https://images.unsplash.com/photo-1526383426426-c1716158d22a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" />
+        <Card.Body className="card-body">
+          <Card.Link className="card-link">
+          <Link className="link" to="users">Add Recipe</Link>  
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      </Col>
+      <Col>
+      <Card className="card">
+        <Card.Img variant="top" className="card-img"
+        src="https://images.unsplash.com/photo-1526383426426-c1716158d22a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" />
+        <Card.Body className="card-body">
+          <Card.Link className="card-link">
+          <Link className="link" to="users">Add Recipe</Link>  
+          </Card.Link>
+        </Card.Body>
+      </Card>
+      </Col>
+      </Row>
+      </Container>
+    </div>
     )
   }
 }
