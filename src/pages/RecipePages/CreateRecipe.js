3ccdcd4f1a959603b0612872e87cb79465796a4d
levilu800b <levilu800b@gmail.com>
2021-03-19 13:00:30 +0000

    package

--- src/pages/RecipePages/CreateRecipe.js
+++ src/pages/RecipePages/CreateRecipe.js
@@ -1,7 +1,7 @@
 // CreateRecipe is found at /user/create-recipe
 // this page has the form to create a recipe
 import React from 'react';
-import AddRecipe from './components/AddRecipe';
+import AddRecipe from '../../components/AddRecipe';
 
 let CreateRecipe = () => {
     return (
