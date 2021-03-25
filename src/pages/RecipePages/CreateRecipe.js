8c356f08f4cf7dcd19537c4c971231624a79ef80
levilu800b <levilu800b@gmail.com>
2021-03-25 15:17:08 +0000

    check

--- src/pages/RecipePages/CreateRecipe.js
+++ src/pages/RecipePages/CreateRecipe.js
@@ -1,21 +1,15 @@
 // CreateRecipe is found at /user/create-recipe
 // this page has the form to create a recipe
- Add-Recipe
 import React from 'react';
 import AddRecipe from '../../components/AddRecipe';
-
 import SideNavBar from '../../components/global/SideNavBar'
- main
 
 let CreateRecipe = (props) => {
     return (
         <>
- Add-Recipe
             <AddRecipe />
-
             <SideNavBar logOut={props.logOutFunc}/>
             <h1>Create Recipe (Recipe Form)</h1>
- main
         </>
     )
 }
