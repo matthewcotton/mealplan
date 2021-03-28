98b6853193fdd9621c77424e6b749126460e27c2
TTBoiMike <michaelthorpe747@hotmail.com>
2021-03-28 19:40:47 +0100

    set up mealplan feed

--- src/pages/MealPlanPages/SingleMealPlan.js
+++ src/pages/MealPlanPages/SingleMealPlan.js
@@ -1,17 +1,34 @@
-import React, {useEffect} from 'react'
-import SideNavBar from '../../components/global/SideNavBar'
+import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+import { DateTime } from "luxon";
+import SideNavBar from '../../components/global/SideNavBar'
+import SingleMealPlanContent from '../../components/Mealplans/SingleMealPlanContent'
 
-let SingleMealPlan = ({apiClient, logOutFunc}) => {
-    let {id} = useParams()
+let SingleMealPlan = ({ apiClient, logOutFunc }) => {
+    const [mealPlanData, setMealPlanData] = useState({})
+    let { id } = useParams()
     
+    const startDate = DateTime.fromISO(mealPlanData.start_date);
+    const endDate = DateTime.fromISO(mealPlanData.end_date);
+    
+    useEffect(() => {
+        apiClient.getSingleMealPlan(id)
+            .then(({ data }) => {
+                console.log(data)
+                setMealPlanData(data)
+            })
+        .catch(console.error)
+        
+    }, [id, apiClient, setMealPlanData])
 
     return (
         <>
             <SideNavBar logOut={logOutFunc} />
-            <Container>
-                <h1>Single Meal Plan</h1>
+            <Container className="mt-5">
+                <h1>{mealPlanData.title}</h1>
+                <h5>{`${mealPlanData.duration} Days | From ${startDate.toLocaleString()} to ${endDate.toLocaleString()}`} </h5>
+                <SingleMealPlanContent recipes={ mealPlanData.recipes} apiClient={apiClient}/>
             </Container>
         </>
     )
